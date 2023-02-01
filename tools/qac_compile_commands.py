4d16dc41c3dbe7a220ce57d4d34df138243f9315
Krzysztof Naglik <krzysztofnaglik96@gmail.com>
2023-02-01 16:08:14 +0100

    Fix QAC compile commands rule [BUILD-506]

---
+++ tools/qac_compile_commands.py
@@ -0,0 +1,30 @@
+import sys, subprocess, json, os
+
+def is_external(path):
+    return path.startswith("external/") or path.find("/bin/external/") != -1
+
+refresh_compile_commands=sys.argv[1]
+workspace_dir = os.getenv('BUILD_WORKSPACE_DIRECTORY')
+
+subprocess.call(refresh_compile_commands, shell=True)
+
+with open(workspace_dir + "/compile_commands.json", "r+") as compile_commands_file:
+    compile_commands = json.load(compile_commands_file)
+
+    for compile_command in compile_commands:
+        args = compile_command["arguments"]
+        for i in range(len(args)):
+            if (args[i] == "-iquote" or args[i] == "-I") and is_external(args[i+1]):
+                args[i] = "-isystem"
+            elif args[i] == "-isystem" and not is_external(args[i+1]):
+                args[i] = "-I"
+
+    compile_commands_file.truncate(0)
+    compile_commands_file.seek(0)
+    json.dump(
+        compile_commands,
+        compile_commands_file,
+        indent=2,
+        check_circular=False
+    )
+
--- tools/qac_compile_commands.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# This script turns the -isystem flag into the -I flag.
-# This is needed for Helix QAC to work properly.
-# Usage: qac_compile_commands <REFRESH_COMPILE_COMMANDS>
-
-REFRESH_COMPILE_COMMANDS=$1
-
-$REFRESH_COMPILE_COMMANDS
-sed -i 's/-isystem/-I/' $BUILD_WORKSPACE_DIRECTORY/compile_commands.json
