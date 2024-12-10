ea8076f2ba5b94a571d7d4e52e299edbc84b14d8
Sebastian Müller <smuller@swift-nav.com>
2024-12-10 17:38:48 +0100

    Fix shellcheck smells

--- clang_format/run_clang_format.sh
+++ clang_format/run_clang_format.sh
@@ -5,20 +5,20 @@
 set -ue
 
 format_all() {
-    CLANG_FORMAT_CONFIG=$(realpath $1)
+    CLANG_FORMAT_CONFIG=$(realpath "$1")
 
-    cd $BUILD_WORKSPACE_DIRECTORY
+    cd "$BUILD_WORKSPACE_DIRECTORY"
     git ls-files '*.[ch]' '*.cpp' '*.cxx' '*.cc' '*.hpp' '*.hxx' \
-    | xargs -r $CLANG_FORMAT_BIN -i --style=file:$CLANG_FORMAT_CONFIG
+    | xargs -r "$CLANG_FORMAT_BIN" -i --style=file:"$CLANG_FORMAT_CONFIG"
 }
 
 format_diff() {
-    CLANG_FORMAT_CONFIG=$(realpath $1)
+    CLANG_FORMAT_CONFIG=$(realpath "$1")
 
-    cd $BUILD_WORKSPACE_DIRECTORY
+    cd "$BUILD_WORKSPACE_DIRECTORY"
     git describe --tags --abbrev=0 --always \
-    | xargs -rI % git diff --diff-filter=ACMRTUXB --name-only --line-prefix=`git rev-parse --show-toplevel`/ % -- '*.[ch]' '*.cpp' '*.cxx' '*.cc' '*.hpp' '*.hxx' \
-    | xargs -r $CLANG_FORMAT_BIN -i --style=file:$CLANG_FORMAT_CONFIG
+    | xargs -rI % git diff --diff-filter=ACMRTUXB --name-only --line-prefix="$(git rev-parse --show-toplevel)"/ % -- '*.[ch]' '*.cpp' '*.cxx' '*.cc' '*.hpp' '*.hxx' \
+    | xargs -r "$CLANG_FORMAT_BIN" -i --style=file:"$CLANG_FORMAT_CONFIG"
 }
 
 check_file() {
@@ -28,14 +28,14 @@ check_file() {
 
     # .clang-format config file has to be placed in the current working directory
     if [ ! -f ".clang-format" ]; then
-        ln -s $CONFIG .clang-format
+        ln -s "$CONFIG" .clang-format
     fi
 
-    $CLANG_FORMAT_BIN $INPUT --dry-run -Werror > $OUTPUT
+    $CLANG_FORMAT_BIN "$INPUT" --dry-run -Werror > "$OUTPUT"
 }
 
 ARG=$1
-CLANG_FORMAT_BIN=$(realpath $2)
+CLANG_FORMAT_BIN=$(realpath "$2")
 shift 2
 
 if [ "$ARG" == "format_diff" ]; then
