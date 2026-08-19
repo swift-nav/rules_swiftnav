ff0dbc324b5b879126a4ec1966a6dd7a1889b1ea
martin4861 <martin.altenburg@swift-nav.com>
2026-08-19 16:03:56 +0200

    Add integration test for local_includes

---
+++ examples/small_world/src/local_includes/include/local_includes/limit.hpp
@@ -0,0 +1,10 @@
+#ifndef SMALL_WORLD_LOCAL_INCLUDES_LIMIT_HPP
+#define SMALL_WORLD_LOCAL_INCLUDES_LIMIT_HPP
+
+namespace small_world {
+
+int limit();
+
+}  // namespace small_world
+
+#endif  // SMALL_WORLD_LOCAL_INCLUDES_LIMIT_HPP
